8fbbd324c9883f5c617d49a2a0bb2eb1baace5cf
ACE User <aceuser@e140-s01.lab.lagcc>
2017-06-07 19:18:50 -0400

    preformed operators and if/else statements

--- js/script.js
+++ js/script.js
@@ -1,1 +1,63 @@
-console.log("This Really Works Bruh! ")
+// // This creates the global variable x and assigns it the number value of 5 
+// var x = 5;
+// console.log(x);
+// x += 1;
+// console.log(x);
+// x++;
+// console.log(x);
+// x = x + 1;
+// console.log(x);
+// // These are three different ways to increment the value of x by 1
+
+// var y = 8;
+// y -= 1;
+// y--;
+// y = y - 1;
+// console.log(y)
+// // these are three different ways to decrease the value of y by 1
+
+// var z = 8;
+// z = z * 2;
+// console.log(z);
+// z *= 2;
+// console.log(z);
+// // these are two ways to multiply the value of z by 2
+
+
+// var a = 8;
+// a = a / 2;
+// console.log(a);
+// a /= 2;
+// console.log(a);
+// // These are 2 ways to divide a by 2 in place
+
+// var b = 17;
+// b = b % 2;
+// console.log(b);
+// // modulus take the value divides it by the number and returns the remainder 
+
+
+function evenOrOdd(num) {
+	var tester = num % 2;
+		if(tester === 0) {
+			console.log("The value passed is even");
+		} else if (tester === 1) {
+			console.log("The value passed is odd")
+		} else {
+				console.log("Please provide a number for this function");
+		}
+}
+
+evenOrOdd(10);
+
+
+
+
+
+
+
+
+
+
+
+
